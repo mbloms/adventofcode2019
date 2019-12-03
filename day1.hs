b0822802e844c2a5a1b1d1bf8cd89a6ffa622a92
Mikael Blomstrand <mikael@mbloms.se>
2019-12-03 11:04:08 +0100

    day 1, part 2

--- day1.hs
+++ day1.hs
@@ -4,7 +4,12 @@ import Data.List
 mass :: Int -> Int
 mass m = (m `div` 3) - 2
 
+includeFuel f = addFuel 0 f
+
+addFuel sum f | f <= 0 = sum
+addFuel sum f = addFuel (sum + f) (mass f)
+
 readInt :: String -> Int
 readInt = read
 
-main = fmap (sum . map (mass . readInt) . words) getContents >>= print
+main = fmap (sum . map (includeFuel . mass . readInt) . words) getContents >>= print
